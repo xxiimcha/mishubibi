0ef5268cb79076c8212681f21939834d178a79fe
Charmaine Cator <xxiimcha@users.noreply.github.com>
2024-05-15 12:24:36 +0800

    Contain footer in a separate file

---
+++ include/footer.php
@@ -0,0 +1,84 @@
+<div class="uren-footer_area">
+    <div class="footer-middle_area">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="footer-widgets_info">
+                        <div class="footer-widgets_logo">
+                            <a href="#">
+                                <img src="assets/images/favicon.ico" alt="Uren's Footer Logo">
+                            </a>
+                        </div>
+                        <div class="widgets-essential_stuff">
+                            <ul>
+                                <li class="uren-address"><span>Address:</span> 75F Avenue, 
+                                Langkaan Street, Bagong Silangan, Naic, Cavite, Philippines</li>
+                                <li class="uren-phone"><span>Call
+                                        Us:</span> <a href="tel://+123123321345">(02) 5481 8894/(0994) 0184 1547</a>
+                                </li>
+                                <li class="uren-email"><span>Email:</span> <a href="mailto://info@yourdomain.com">contactus@mishubibimotors.ph</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-8">
+                    <div class="footer-widgets_area">
+                        <div class="row">
+                            <div class="col-lg-3 col-md-6">
+                                <div class="footer-widgets_title">
+                                    <h3>Information</h3>
+                                </div>
+                                <div class="footer-widgets">
+                                    <ul>
+                                        <li><a href="javascript:void(0)">About Us</a></li>
+                                        <li><a href="javascript:void(0)">Delivery Information</a></li>
+                                        <li><a href="javascript:void(0)">Privacy Policy</a></li>
+                                        <li><a href="javascript:void(0)">Terms & Conditions</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-6">
+                                <div class="footer-widgets_title">
+                                    <h3>Customer Service</h3>
+                                </div>
+                                <div class="footer-widgets">
+                                    <ul>
+                                        <li><a href="javascript:void(0)">Contact Us</a></li>
+                                        <li><a href="javascript:void(0)">Returns</a></li>
+                                        <li><a href="javascript:void(0)">Site Map</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-6">
+                                <div class="footer-widgets_title">
+                                    <h3>Extras</h3>
+                                </div>
+                                <div class="footer-widgets">
+                                    <ul>
+                                        <li><a href="javascript:void(0)">About Us</a></li>
+                                        <li><a href="javascript:void(0)">Delivery Information</a></li>
+                                        <li><a href="javascript:void(0)">Privacy Policy</a></li>
+                                        <li><a href="javascript:void(0)">Terms & Conditions</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-6">
+                                <div class="footer-widgets_title">
+                                    <h3>My Account</h3>
+                                </div>
+                                <div class="footer-widgets">
+                                    <ul>
+                                        <li><a href="javascript:void(0)">My Account</a></li>
+                                        <li><a href="javascript:void(0)">Order History</a></li>
+                                        <li><a href="javascript:void(0)">Wish List</a></li>
+                                        <li><a href="javascript:void(0)">Newsletter</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
