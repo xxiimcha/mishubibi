37cc61648fc54987ac791c146014626d823be6fd
Charmaine Cator <xxiimcha@users.noreply.github.com>
2024-05-18 12:20:01 +0800

    brand new cars viewable

---
+++ pages/view_car.php
@@ -0,0 +1,221 @@
+<?php 
+include('../controller/config.php');
+include('../include/head.php');
+
+// Get the car ID from the URL
+$carId = isset($_GET['id']) ? intval($_GET['id']) : 0;
+
+// Fetch car details from the database
+$sql = "SELECT * FROM cars WHERE id = $carId";
+$result = $conn->query($sql);
+$car = $result->fetch_assoc();
+
+if (!$car) {
+    echo "Car not found!";
+    exit;
+}
+?>
+
+<body class="template-color-1">
+    <div class="main-wrapper">
+        <?php include('../include/header.php');?>
+        <div class="breadcrumb-area">
+            <div class="container">
+                <div class="breadcrumb-content">
+                    <h2><?php echo htmlspecialchars($car['vehicle_name']); ?></h2>
+                    <ul>
+                        <li><a href="index.html">Home</a></li>
+                        <li class="active"><?php echo htmlspecialchars($car['vehicle_name']); ?></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="sp-area">
+            <div class="container-fluid">
+                <div class="sp-nav">
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <div class="sp-img_area">
+                                <div class="sp-img_slider slick-img-slider uren-slick-slider" data-slick-options='{
+                                "slidesToShow": 1,
+                                "arrows": false,
+                                "fade": true,
+                                "draggable": false,
+                                "swipe": true,
+                                "asNavFor": ".sp-img_slider-nav"
+                                }'>
+                                    <div class="single-slide zoom">
+                                        <img src="<?php echo htmlspecialchars($car['car_image']); ?>" alt="<?php echo htmlspecialchars($car['vehicle_name']); ?>">
+                                    </div>
+                                </div>
+                                <div class="sp-img_slider-nav slick-slider-nav uren-slick-slider slider-navigation_style-3" data-slick-options='{
+                                    "slidesToShow": 3,
+                                    "asNavFor": ".sp-img_slider",
+                                    "focusOnSelect": true,
+                                    "arrows" : true,
+                                    "spaceBetween": 30
+                                }' data-slick-responsive='[
+                                        {"breakpoint":1501, "settings": {"slidesToShow": 3}},
+                                        {"breakpoint":992, "settings": {"slidesToShow": 4}},
+                                        {"breakpoint":768, "settings": {"slidesToShow": 3}},
+                                        {"breakpoint":575, "settings": {"slidesToShow": 2}}
+                                    ]'>
+                                    <!-- Add more images if available -->
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-8">
+                            <div class="sp-content">
+                                <div class="sp-heading">
+                                    <h5><?php echo htmlspecialchars($car['vehicle_name']); ?></h5>
+                                </div>
+                                <span class="reference">Reference: <?php echo htmlspecialchars($car['id']); ?></span>
+                                <div class="rating-box">
+                                    <ul>
+                                        <li><i class="ion-android-star"></i></li>
+                                        <li><i class="ion-android-star"></i></li>
+                                        <li><i class="ion-android-star"></i></li>
+                                        <li class="silver-color"><i class="ion-android-star"></i></li>
+                                        <li class="silver-color"><i class="ion-android-star"></i></li>
+                                    </ul>
+                                </div>
+                                <div class="sp-essential_stuff">
+                                    <ul>
+                                        <li>Brand: <a href="javascript:void(0)"><?php echo htmlspecialchars($car['brand']); ?></a></li>
+                                        <li>Vehicle Type: <a href="javascript:void(0)"><?php echo htmlspecialchars($car['vehicle_type']); ?></a></li>
+                                        <li>Power: <a href="javascript:void(0)"><?php echo htmlspecialchars($car['power']); ?></a></li>
+                                    </ul>
+                                </div>
+                                <div class="uren-tag-line">
+                                    <h6>Tags:</h6>
+                                    <a href="javascript:void(0)">vehicle</a>,
+                                    <a href="javascript:void(0)">car</a>,
+                                    <a href="javascript:void(0)">bike</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="sp-product-tab_area">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="sp-product-tab_nav">
+                            <div class="product-tab">
+                                <ul class="nav product-menu">
+                                    <li><a class="active" data-toggle="tab" href="#description"><span>Description</span></a>
+                                    </li>
+                                    <li><a data-toggle="tab" href="#specification"><span>Specification</span></a></li>
+                                    <li><a data-toggle="tab" href="#reviews"><span>Reviews (1)</span></a></li>
+                                </ul>
+                            </div>
+                            <div class="tab-content uren-tab_content">
+                                <div id="description" class="tab-pane active show" role="tabpanel">
+                                    <div class="product-description">
+                                        <ul>
+                                            <li>
+                                                <strong>Description</strong>
+                                                <span><?php echo nl2br(htmlspecialchars($car['description'])); ?></span>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div id="specification" class="tab-pane" role="tabpanel">
+                                    <table class="table table-bordered specification-inner_stuff">
+                                        <tbody>
+                                            <tr>
+                                                <td>Fuel Tank Capacity</td>
+                                                <td><?php echo htmlspecialchars($car['fuel_tank_capacity']); ?></td>
+                                            </tr>
+                                            <tr>
+                                                <td>Engine</td>
+                                                <td><?php echo htmlspecialchars($car['engine']); ?></td>
+                                            </tr>
+                                            <tr>
+                                                <td>Power</td>
+                                                <td><?php echo htmlspecialchars($car['power']); ?></td>
+                                            </tr>
+                                            <!-- Add more specifications as needed -->
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <div id="reviews" class="tab-pane" role="tabpanel">
+                                    <div class="tab-pane active" id="tab-review">
+                                        <form class="form-horizontal" id="form-review">
+                                            <div id="review">
+                                                <table class="table table-striped table-bordered">
+                                                    <tbody>
+                                                        <tr>
+                                                            <td style="width: 50%;"><strong>Customer</strong></td>
+                                                            <td class="text-right">15/09/20</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td colspan="2">
+                                                                <p>Good product! Thank you very much</p>
+                                                                <div class="rating-box">
+                                                                    <ul>
+                                                                        <li><i class="ion-android-star"></i></li>
+                                                                        <li><i class="ion-android-star"></i></li>
+                                                                        <li><i class="ion-android-star"></i></li>
+                                                                        <li><i class="ion-android-star"></i></li>
+                                                                        <li><i class="ion-android-star"></i></li>
+                                                                    </ul>
+                                                                </div>
+                                                            </td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                            <h2>Write a review</h2>
+                                            <div class="form-group required">
+                                                <div class="col-sm-12 p-0">
+                                                    <label>Your Email <span class="required">*</span></label>
+                                                    <input class="review-input" type="email" name="con_email" id="con_email" required>
+                                                </div>
+                                            </div>
+                                            <div class="form-group required second-child">
+                                                <div class="col-sm-12 p-0">
+                                                    <label class="control-label">Share your opinion</label>
+                                                    <textarea class="review-textarea" name="con_message" id="con_message"></textarea>
+                                                    <div class="help-block"><span class="text-danger">Note:</span> HTML is not translated!</div>
+                                                </div>
+                                            </div>
+                                            <div class="form-group last-child required">
+                                                <div class="col-sm-12 p-0">
+                                                    <div class="your-opinion">
+                                                        <label>Your Rating</label>
+                                                        <span>
+                                                            <select class="star-rating">
+                                                                <option value="1">1</option>
+                                                                <option value="2">2</option>
+                                                                <option value="3">3</option>
+                                                                <option value="4">4</option>
+                                                                <option value="5">5</option>
+                                                            </select>
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                                <div class="uren-btn-ps_right">
+                                                    <button class="uren-btn-2">Continue</button>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div><br><br>
+        <?php include('../include/footer.php');?>          
+    </div>
+<?php include('../include/foot.php');?>
+</body>
+</html>
+
+<?php 
+$conn->close();
+?>
